9d27c5c36ccdc4e0d5b334e56e419c51f723e48e
thereisnomeaning <108408262+thereisnomeaning@users.noreply.github.com>
2024-05-27 21:57:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ load_dotenv()
 BOT_TOKEN = getenv('BOT_TOKEN')
 FOLDER_ID = getenv('FOLDER_ID')
 IEM_TOKEN_INFO = {'IEM_TOKEN': 't1.9euelZqVi8jOzpONi8mPm5STi8uUx-3rnpWax5KYnJ2JjcqQk5ydmsyXz83l8_dTflJN-e90F3oz_N3z9xMtUE3573QXejP8zef1656VmsuTy5GXm5OPi5HKkpzJipWY7_zF656VmsuTy5GXm5OPi5HKkpzJipWYveuelZqJnJCRjo_JjpjPio2XmszOj7XehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.RLCQmxeXyRdamaVJPvsit1vEQSypBgm0mLiBuuGHVWI5-DtRSkTHyq47ffnrjXSO0rdjU2NpDPakXVpvEYkWCA',
-                  'EXPIRES_IN': time.time() + 40000}
+                  'EXPIRES_IN': 0}
 ADMIN_ID = int(getenv('ADMIN_ID'))
 
 MAX_MODEL_TOKENS = 200
-MAX_USERS = 6
+MAX_USERS = 90
 MAX_STT_BLOCKS_PER_PERSON = 3
-MAX_GPT_TOKENS_PER_PERSON = 1000
-MAX_TTS_TOKENS_PER_PERSON = 1000
+MAX_GPT_TOKENS_PER_PERSON = 2000
+MAX_TTS_TOKENS_PER_PERSON = 2000
 MAX_GPT_TOKENS_PER_MESSAGE = 500
 MAX_TTS_TOKENS_PER_MESSAGE = 500
 
-SYSTEM_PROMPT = '''Ты дружелюбный англичанин-ассистент. Коротко поддерживай беседу и задавай вопрос в конце.'''
+SYSTEM_PROMPT = '''Ты дружелюбный англичанин-ассистент. Коротко поддерживай беседу и задавай вопрос в конце. Отвечай на английском'''
 
 SYSTEM_PROMPT_TRANSLATION = '''Переведи сообщение на русский.'''
 
@@ -83,8 +83,8 @@ class GPT:
         "modelUri": f"gpt://{FOLDER_ID}/yandexgpt-lite",  # модель для генерации текста
         "completionOptions": {
             "stream": False,  # потоковая передача частично сгенерированного текста выключена
-            "temperature": 0.6,  # чем выше значение этого параметра, тем более креативными будут ответы модели (0-1)
-            "maxTokens": "50"  # максимальное число сгенерированных токенов
+            "temperature": 0.3,  # чем выше значение этого параметра, тем более креативными будут ответы модели (0-1)
+            "maxTokens": "150"  # максимальное число сгенерированных токенов
         },
         "messages": [
             {
